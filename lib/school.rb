0d0f267a362e5fc88ce57f9c5b7b52efaee095db
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-19 20:54:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,9 +24,9 @@ class School
   end
 
   def sort
-  @roster.map do |grade,student_names| 
-    puts grade
-    puts student_names.sort
+  @roster.map do |grade,student_names|
+    grade
+    student_names.sort
   end
   end
 
